aefb6096db18cbae0f4595d981f3ead962d73bee
Ian Gall <ianpgall@gmail.com>
2013-06-29 19:13:42 -0400

    Tried to condense, and added support for an object of events/callbacks, always supporting both normal and delegated handlers

--- event-delegation.js
+++ event-delegation.js
@@ -1,26 +1,42 @@
-var addEvent = (function () {
+(function (w) {
     "use strict";
-    
-    var executeCallback = function (callbackFunction, context, evnt) {
-        var result = Function.prototype.call.call(callbackFunction, context, evnt);
+
+    var F, O, executeCallback, generateNormalHandler,
+        generateDelegateHandler, bindChooser, handleBinding, exposedHandler;
+
+    F = function () {};
+    O = {};
+
+    executeCallback = function (callbackFunction, context, evnt) {
+        var result;
+        result = F.call.call(callbackFunction, context, evnt);
         if (result === false) {
             if (evnt.preventDefault) {
                 evnt.preventDefault();
+            } else {
+                evnt.returnValue = false;
             }
-            evnt.returnValue = false;
         }
-    }, generateNormalHandler = function (callb) {
-        return function (e) {
+    };
+
+    generateNormalHandler = function (callb) {
+        var func;
+        func = function (e) {
             e = e || window.event;
             executeCallback(callb, this, e);
         };
-    }, generateDelegateHandler = function (sel, callb) {
-        return function (e) {
+        return func;
+    };
+
+    generateDelegateHandler = function (sel, callb) {
+        var func;
+        func = function (e) {
+            var target, i, isMatch, matches, matchesLength;
             e = e || window.event;
-            var target = e.target || e.srcElement,
-                i, cur, isMatch = false,
-                matches = this.querySelectorAll(sel),
-                matchesLength = matches.length;
+            target = e.target || e.srcElement;
+            isMatch = false;
+            matches = this.querySelectorAll(sel);
+            matchesLength = matches.length;
             while (target && target !== this) {
                 for (i = 0; !isMatch && i < matchesLength; i++) {
                     if (matches[i] === target) {
@@ -35,32 +51,50 @@ var addEvent = (function () {
                 }
             }
         };
-    }, actualHandler = (function () {
-        var binder = (function () {
-            var tester = document.createElement("div");
-            if (tester.addEventListener) {
-                return function (el, en, cb) {
-                    el.addEventListener(en, cb, false);
-                };
-            } else if (tester.attachEvent) {
-                return function (el, en, cb) {
-                    el.attachEvent("on" + en, cb);
-                };
+        return func;
+    };
+
+    bindChooser = (function () {
+        var tester, binder;
+        tester = document.createElement("div");
+        if (tester.addEventListener) {
+            binder = function (el, en, cb) {
+                el.addEventListener(en, cb, false);
+            };
+        } else if (tester.attachEvent) {
+            binder = function (el, en, cb) {
+                el.attachEvent("on" + en, cb);
+            };
+        } else {
+            binder = function () {};
+        }
+        return binder;
+    }());
+
+    handleBinding = function (el, en, fo, cb) {
+        var han;
+        if (typeof fo === "string" && fo) {
+            // Delegated
+            han = generateDelegateHandler(fo, cb);
+        } else {
+            // Normal
+            han = generateNormalHandler(cb || fo);
+        }
+        bindChooser(el, en, han);
+    };
+
+    exposedHandler = function (element, eventName, filterOn, callback) {
+        var name, evtObj, curCallback;
+        if (O.toString.call(eventName) === "[object Object]") {
+            evtObj = eventName;
+            for (name in evtObj) {
+                curCallback = evtObj[name];
+                handleBinding(element, name, filterOn, curCallback);
             }
-        })(), mainHandler = function (element, eventName, filterOn, callback) {
-            var handler;
-            if (typeof filterOn === "string" && filterOn) {
-                // Delegate
-                handler = generateDelegateHandler(filterOn, callback);
-            } else {
-                // Normal
-                handler = generateNormalHandler(filterOn ? filterOn : callback);
-            }
-            binder(element, eventName, handler);
-        };
-        
-        return mainHandler;
-    })();
-    
-    return actualHandler;
-})();
+        } else {
+            handleBinding(element, eventName, filterOn, callback);
+        }
+    };
+
+    w.addEvent = exposedHandler;
+}(window));
